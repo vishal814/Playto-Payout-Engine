66db78b3cd6a41a29da74bd8c84e43618333b349
Vishal bhojwani <vishalbhojwani917@gmail.com>
2026-04-28 16:22:25 +0530

    Fix celery broken pipe on railway

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start the Celery background worker in the background (using the & symbol)
-celery -A backend worker -l info &
+celery -A backend worker -l info -P solo &
 
 # Start the Django web server in the foreground
 gunicorn backend.wsgi --bind 0.0.0.0:$PORT --log-file -
